d136522de72bb7cf1c5be3db7399513e79b290c6
Celtuzed <uter222222@gmail.com>
2022-02-11 17:03:24 +0300

    Reworked the function

--- predict_salary.py
+++ predict_salary.py
@@ -1,12 +1,13 @@
 def predict_rub_salary(payment_from, payment_to):
 
-    salary_from_the_vacancy = None
-
     if payment_from and payment_to:
         salary_from_the_vacancy = (payment_from + payment_to)/2
+        return salary_from_the_vacancy
+
     elif payment_from and not payment_to:
         salary_from_the_vacancy = payment_from*1.2
+        return salary_from_the_vacancy
+
     elif payment_to and not payment_from:
         salary_from_the_vacancy = payment_to*0.8
-
-    return salary_from_the_vacancy
+        return salary_from_the_vacancy
