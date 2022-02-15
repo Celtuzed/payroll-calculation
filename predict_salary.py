124790940c3cb7febad2d43bb029a65a65c6750f
Celtuzed <uter222222@gmail.com>
2022-02-15 19:17:26 +0300

    Deleted excess code

--- predict_salary.py
+++ predict_salary.py
@@ -1,13 +1,10 @@
 def predict_rub_salary(payment_from, payment_to):
 
     if payment_from and payment_to:
-        salary_from_the_vacancy = (payment_from + payment_to)/2
-        return salary_from_the_vacancy
+        return (payment_from + payment_to)/2
 
     elif payment_from and not payment_to:
-        salary_from_the_vacancy = payment_from*1.2
-        return salary_from_the_vacancy
+        return payment_from*1.2
 
     elif payment_to and not payment_from:
-        salary_from_the_vacancy = payment_to*0.8
-        return salary_from_the_vacancy
+        return payment_to*0.8
