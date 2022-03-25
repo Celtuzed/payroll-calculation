743f9961d3b7f0db293e934c3407e8af15fd9e76
Celtuzed <76615478+Celtuzed@users.noreply.github.com>
2022-03-25 18:44:38 +0300

    Reduce the code

--- main.py
+++ main.py
@@ -7,21 +7,11 @@ from superjob import get_vacancies_for_superjob
 from headhunter import get_vacancies_for_headhunter
 
 
-def get_tables(sj_vacancies, hh_vacancies, languages):
+def get_table(sj_vacancies, hh_vacancies, languages):
 
-    hh_title = "HeadHunter Moscow"
-    sj_title = "SuperJob Moscow"
+    title = "HeadHunter and SuperJob Moscow"
 
-    sj_table_data = [
-        (
-            'Язык программирования',
-            'Вакансий найдено',
-            'Вакансий обработано',
-            'Средняя зарплата'
-        )
-    ]
-
-    hh_table_data = [
+    table_data = [
         (
             'Язык программирования',
             'Вакансий найдено',
@@ -32,28 +22,27 @@ def get_tables(sj_vacancies, hh_vacancies, languages):
 
     for language in languages:
 
-        sj_table_data.append(
+        table_data.append(
             (
-                language,
+                f"sj_{language}",
                 sj_vacancies[language]['vacancies_found'],
                 sj_vacancies[language]['vacancies_processed'],
                 sj_vacancies[language]['average_salary']
             )
         )
 
-        hh_table_data.append(
+        table_data.append(
             (
-                language,
+                f"hh_{language}",
                 hh_vacancies[language]['vacancies_found'],
                 hh_vacancies[language]['vacancies_processed'],
                 hh_vacancies[language]['average_salary']
             )
         )
 
-    sj_table = SingleTable(sj_table_data, sj_title)
-    hh_table = SingleTable(hh_table_data, hh_title)
+    table = SingleTable(table_data, title)
 
-    return sj_table.table, hh_table.table
+    return table.table
 
 
 if __name__ == '__main__':
@@ -75,7 +64,6 @@ if __name__ == '__main__':
 
     hh_vacancies = get_vacancies_for_headhunter(languages)
     sj_vacancies = get_vacancies_for_superjob(sj_token, languages)
-    sj_table, hh_table = get_tables(sj_vacancies, hh_vacancies, languages)
+    table = get_table(sj_vacancies, hh_vacancies, languages)
 
-    print(sj_table)
-    print(hh_table)
+    print(table)
--- predict_salary.py
+++ predict_salary.py
@@ -3,8 +3,8 @@ def predict_rub_salary(payment_from, payment_to):
     if payment_from and payment_to:
         return (payment_from + payment_to)/2
 
-    elif payment_from and not payment_to:
+    elif not payment_to:
         return payment_from*1.2
 
-    elif payment_to and not payment_from:
+    elif not payment_from:
         return payment_to*0.8
